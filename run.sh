15011d07fcef50ede52edc91dc197be4bd642f75
sandeeprv <rvsandeep@outlook.com>
2018-10-30 06:17:11 -0700

    python3 package issues while testing link

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 #
 # Use this shell script to compile (if necessary) your code and then execute it. Below is an example of what might be found in this file if your program was written in Python
 #
-python ./src/runner.py ./input/h1b_input.csv ./output/top_10_occupations.txt ./output/top_10_states.txt
+python3 ./src/runner.py ./input/h1b_input.csv ./output/top_10_occupations.txt ./output/top_10_states.txt
