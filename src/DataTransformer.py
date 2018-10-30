617f7b6e32d83cfd68e6b6850b8f151751ea099b
sandeeprv <rvsandeep@outlook.com>
2018-10-30 07:21:46 -0700

    minor comments

--- src/DataTransformer.py
+++ src/DataTransformer.py
@@ -2,10 +2,12 @@ import json
 import os
 RESOURCE_DIR = 'src/resources/'
 
+#update old column names to a new convention
 def standardize_col_names(raw_data):
     fileformats = os.listdir(RESOURCE_DIR)
     standard_mapping = {}
     for fileformat in fileformats:
         new_mapping = json.loads(open(RESOURCE_DIR+fileformat).read())
+        #use the latest mapping
         standard_mapping = {**standard_mapping, **new_mapping}
     return raw_data.rename_columns(columns = standard_mapping)
